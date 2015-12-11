492823b845144db6966516d6b7b74dfbdb72f260
mzxdream <mzxdream@gmail.com>
2015-12-11 20:35:12 +0800

    1

---
+++ src/shared/net/m_event_dispatcher.h
@@ -0,0 +1,15 @@
+#ifndef _M_EVENT_DISPATCHER_H_
+#define _M_EVENT_DISPATCHER_H_
+
+class MEventDispatcher
+{
+public:
+    MEventDispatcher();
+    ~MEventDispatcher();
+    MEventDispatcher(const MEventDispatcher &) = delete;
+    MEventDispatcher& operator=(const MEventDispatcher &) = delete;
+private:
+
+};
+
+#endif
