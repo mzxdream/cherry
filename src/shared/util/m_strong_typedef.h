9bf1db3b87ae09db4ef20e0b7802ad9141a25bc5
mzxdream <mzxdream@gmail.com>
2016-04-27 17:39:00 +0800

    1

--- src/shared/util/m_strong_typedef.h
+++ src/shared/util/m_strong_typedef.h
@@ -1,34 +1,21 @@
 #ifndef _M_STRONG_TYPEDEF_H_
 #define _M_STRONG_TYPEDEF_H_
 
-#include <type_traits>
-
-#define MSTRONG_TYPEDEF(T, U) \
-    typedef MStrongTypedef<T, MStrongTypedef_##OldType2##NewType> NewType;
-
-template <typename T, typename U>
-class MStrongTypedef
-{
-public:
-    explicit MStrongTypedef(const T &t)
-        :t_(t)
-    {
-    }
-    MStrongTypedef(const MStrongTypedef &rhs)
-        :t_(rhs.t_)
-    {
-    }
-    MStrongTypedef& operator=(const T &rhs)
-    {
-
-    }
-    MStrongTypedef& operator=(const MStrongTypedef &rhs)
-    {
-        t_ = rhs.t_;
-        return *this;
-    }
-private:
-    T t_;
+#define MSTRONG_TYPEDEF(T, U)                                 \
+class U                                                       \
+{                                                             \
+public:                                                       \
+    explicit U(const T &t) : t_(t) {};                        \
+    explicit U(T &&t) : t_(std::move(t)) {};                  \
+    template <typename ...Args>                               \
+    explicit U(Args ...args) : t_(args...) {};                \
+                                                              \
+    U& operator=(const T &t) { t_ = t; return *this; };       \
+                                                              \
+    operator T&() { return t_; };                             \
+    operator const T&() const { return t_; };                 \
+private:                                                      \
+    T t_;                                                     \
 };
 
 #endif
