838babb3125e98fcb347c7c9657d14c7467ee3f8
mzxdream <mzxdream@gmail.com>
2016-05-28 17:57:13 +0800

    1

--- login_server/src/main.cpp
+++ login_server/src/main.cpp
@@ -1,8 +1,43 @@
+#include <signal.h>
 #include <iostream>
 
-#define CONFIG_PATH = "./config";
+static bool stop_flag = false;
 
-int main(int argc, char *argv[])
+void OnSignal(int s)
 {
+    std::cout << "OnSignal:" << s << std::endl;
+    switch (s)
+    {
+    case SIGINT:
+    case SIGTERM:
+        stop_flag = true;
+        break;
+    default:
+        break;
+    }
+    signal(s, OnSignal);
+}
+
+void HookSignals()
+{
+    signal(SIGINT, OnSignal);
+    signal(SIGTERM, OnSignal);
+}
+
+void UnhookSignals()
+{
+    signal(SIGINT, 0);
+    signal(SIGTERM, 0);
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << "---------begin-------" << std::endl;
+    HookSignals();
+    while (!stop_flag)
+    {
+    }
+    UnhookSignals();
+    std::cout << "----------end--------" << std::endl;
     return 0;
 }
