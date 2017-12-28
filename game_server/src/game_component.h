53c19f422024344aa799bca5a14a90d10624b736
mzxdream <mzxdream@gmail.com>
2017-12-28 19:06:53 +0800

    1

---
+++ game_server/src/game_component.h
@@ -0,0 +1,13 @@
+#ifndef __GAME_COMPONENT_H__
+#define __GAME_COMPONENT_H__
+
+#include "game_object.h"
+
+class GameComponent
+    : public GameObject
+{
+public:
+ A();
+};
+
+#endif
---
+++ game_server/src/game_object.h
@@ -0,0 +1,10 @@
+#ifndef __GAME_OBJECT_H__
+#define __GAME_OBJECT_H__
+
+class GameObject
+{
+public:
+    virtual ~GameObject() = 0;
+};
+
+#endif
