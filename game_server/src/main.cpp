8ef8bd871c6a8c87524fcf119f042096fac1f6a8
mzxdream <mzxdream@gmail.com>
2018-01-18 11:27:39 +0800

    1

--- game_server/src/main.cpp
+++ game_server/src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <unistd.h>
+#include <mzx/cmd_manager.h>
 
 int main(int argc, char *argv[])
 {
+    mzx::CmdManager cmd_manager;
+    cmd_manager.Start();
+    int i = 1;
+    cmd_manager.RegistCmd("111111", []() {
+        std::cout << "receive 1" << std::endl;
+    });
+    cmd_manager.RegistCmd("222222", [&]() {
+        std::cout << "receive 2" << std::endl;
+        i = 0;
+    });
+    while (i != 0)
+    {
+        cmd_manager.ExcuteCmd();
+        sleep(1);
+    }
+    cmd_manager.StopAndJoin();
     return 0;
 }
