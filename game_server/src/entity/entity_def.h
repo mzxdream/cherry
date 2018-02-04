c2b4eac107b3b4cc82040521ac1cc113c17ce8ca
mzxdream <mzxdream@gmail.com>
2018-02-04 08:19:52 -0500

    1

---
+++ game_server/src/entity/entity_def.h
@@ -0,0 +1,6 @@
+#ifndef __CHERRY_ENTITY_DEF_H__
+#define __CHERRY_ENTITY_DEF_H__
+
+
+
+#endif
---
+++ game_server/src/entity/entity_factory.h
@@ -0,0 +1,12 @@
+#ifndef __CHERRY_ENTITY_FACTORY_H__
+#define __CHERRY_ENTITY_FACTORY_H__
+
+namespace cherry {
+
+class EntityFactory
+{
+};
+
+}
+
+#endif
